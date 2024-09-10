a0dcf46627bb71436082ccd8edaf3421f58a6205
ulda3001 <ulda3001@TRLC2Z8JR3>
2024-09-10 16:24:56 +0300

    Dto class and view components

--- Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.AboutViewComponents
 {
     public class _AboutUsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _client;
+
+        public _AboutUsComponentPartial(IHttpClientFactory client)
         {
+            _client = client;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _client.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7210/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
---
+++ Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
